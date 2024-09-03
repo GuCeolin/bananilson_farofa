d50022e3d9e85cebe751743f18430f9453c84bed
djacerola <129908336+GuCeolin@users.noreply.github.com>
2024-09-02 23:56:15 -0300

    Update Main.java

--- code/Main.java
+++ code/Main.java
@@ -1,45 +1,71 @@
-
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         // Criando disciplinas
+        Disciplina MecanicaDosFluidos = new Disciplina("Mecanica Dos Fluidos", 4, 9.0);
+        Disciplina fisica = new Disciplina("Física", 4, 8.5);
+        Disciplina mecanicaAutomotiva = new Disciplina("Mecânica Automotiva", 3, 0);
         Disciplina matematica = new Disciplina("Matemática", 4, 0);
-        Disciplina fisica = new Disciplina("Física", 4, 0);
+        Disciplina quimica = new Disciplina("Química", 4, 0);
+        Disciplina eletromagnetismo = new Disciplina("Eletromagnetismo", 4, 0);
+        Disciplina estatistica = new Disciplina("Estatistica", 2, 0);
+        Disciplina Carburadores = new Disciplina("Carburadores", 2, 0);
 
-        // Criando currículo e curso
-        Curriculo curriculoJoao = new Curriculo();
-        Curso cursoEngenharia = new Curso("Engenharia", List.of(matematica, fisica));
+        // Criando curso
+        Curso engenharias = new Curso("Engenharia", Arrays.asList(MecanicaDosFluidos, fisica));
 
-        // Criando aluno e professor
-        Aluno joao = new Aluno("João", "12345", curriculoJoao);
-        Professor professorCarlos = new Professor("Carlos", List.of(matematica, fisica));
+        // Criando professor
+        Professor profJoacir = new Professor("Joacir", Arrays.asList(MecanicaDosFluidos));
 
-        
+        // Criando aluno
+        Curriculo curriculoAluno = new Curriculo();
+        Aluno alunoJamal = new Aluno("Jamal", "0237", curriculoAluno);
+
+
         // Criando turma
-        Turma turmaMatematica = new Turma(matematica, professorCarlos);
+        Turma turmaMatematica = new Turma(MecanicaDosFluidos, profJoacir);
 
-        // Criando dados da universidade e secretaria
+        // Criando aluno
+        Aluno alunoPedro = new Aluno("Pedro", "0657", curriculoAluno);
+
+        // Matriculando o aluno em disciplinas obrigatórias
+        alunoPedro.matricularDisciplinaObrigatoria(matematica);
+        alunoPedro.matricularDisciplinaObrigatoria(fisica);
+        alunoPedro.matricularDisciplinaObrigatoria(quimica);
+        alunoPedro.matricularDisciplinaObrigatoria(eletromagnetismo);
+
+        // Matriculando o aluno em disciplinas optativas
+        alunoPedro.matricularDisciplinaOptativa(mecanicaAutomotiva);
+        alunoPedro.matricularDisciplinaOptativa(Carburadores);
+
+        // Criando dados da universidade
         DadosUniversidade dadosUniversidade = new DadosUniversidade();
-        Secretaria secretaria = new Secretaria(dadosUniversidade);
-
-        // Adicionando aluno e professor à universidade
-        dadosUniversidade.adicionarAluno(joao);
-        dadosUniversidade.adicionarProfessor(professorCarlos);
-        dadosUniversidade.adicionarCurso(cursoEngenharia);
+        dadosUniversidade.adicionarAluno(alunoJamal);
+        dadosUniversidade.adicionarProfessor(profJoacir);
+        dadosUniversidade.adicionarCurso(engenharias);
         dadosUniversidade.adicionarTurma(turmaMatematica);
 
-        // Matriculando aluno em uma turma
-        secretaria.matricularAlunoEmTurma(joao, turmaMatematica);
+        // Criando secretaria
+        Secretaria secretaria = new Secretaria(dadosUniversidade);
 
-        // Gerando cobrança
+        // Matriculando aluno na turma
+        secretaria.matricularAlunoEmTurma(alunoJamal, turmaMatematica);
+        alunoJamal.matricularDisciplinaObrigatoria(fisica);
+        
+
+        
+        
+
+        // Gerando cobrança para o aluno
         SistemaDeCobrancas sistemaDeCobrancas = new SistemaDeCobrancas();
-        sistemaDeCobrancas.gerarCobranca(joao, 1500.00);
-        sistemaDeCobrancas.notificarAluno(joao);
+        sistemaDeCobrancas.gerarCobranca(alunoJamal, 1500.00);
+        sistemaDeCobrancas.notificarAluno(alunoJamal);
 
-        // Exibindo informações
+        // Exibindo informações da universidade
         System.out.println(dadosUniversidade);
-        System.out.println(secretaria);
-        System.out.println(joao);
+
+        // Exibindo informações da turma
         System.out.println(turmaMatematica);
     }
 }
