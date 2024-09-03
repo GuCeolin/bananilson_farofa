1612bc614f077581e3f10f2c42f0efe3e3578265
djacerola <129908336+GuCeolin@users.noreply.github.com>
2024-09-02 23:55:05 -0300

    Update Aluno.java

--- code/Aluno.java
+++ code/Aluno.java
@@ -1,13 +1,19 @@
-package code;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aluno {
-    private final String nome;
-    private final String matricula;
-    private final Curriculo curriculo;
+    private String nome;
+    private String matricula;
+    private Curriculo curriculo;
+    private List<Disciplina> disciplinasObrigatorias;
+    private List<Disciplina> disciplinasOptativas;
 
     public Aluno(String nome, String matricula, Curriculo curriculo) {
         this.nome = nome;
         this.matricula = matricula;
         this.curriculo = curriculo;
+        this.disciplinasObrigatorias = new ArrayList<>();
+        this.disciplinasOptativas = new ArrayList<>();
     }
 
     public String getNome() {
@@ -22,20 +28,42 @@ public class Aluno {
         return curriculo;
     }
 
-    public void matricularDisciplina(Disciplina disciplina) {
-        curriculo.adicionarDisciplina(disciplina);
+    public List<Disciplina> getDisciplinasObrigatorias() {
+        return disciplinasObrigatorias;
     }
 
-    public double calcularCoeficienteRendimento() {
-        return curriculo.calcularCoeficienteRendimento();
+    public List<Disciplina> getDisciplinasOptativas() {
+        return disciplinasOptativas;
     }
+
+    public void matricularDisciplinaObrigatoria(Disciplina disciplina) {
+        if (disciplinasObrigatorias.size() < 4) {
+            disciplinasObrigatorias.add(disciplina);
+            curriculo.adicionarDisciplina(disciplina);
+        } else {
+            System.out.println("Não é possível matricular em mais disciplinas obrigatórias.");
+        }
+    }
+
+    public void matricularDisciplinaOptativa(Disciplina disciplina) {
+        if (disciplinasOptativas.size() < 2) {
+            disciplinasOptativas.add(disciplina);
+            curriculo.adicionarDisciplina(disciplina);
+        } else {
+            System.out.println("Não é possível matricular em mais disciplinas optativas.");
+        }
+    }
+
+    
+
+    
 
     @Override
     public String toString() {
-        return "Aluno{" +
-                "nome='" + nome + '\'' +
-                ", matricula='" + matricula + '\'' +
-                ", curriculo=" + curriculo +
-                '}';
+        return "Aluno: " + nome + "\n" +
+               "Matrícula: " + matricula + "\n" +
+               "Disciplinas Obrigatórias: " + disciplinasObrigatorias + "\n" +
+               "Disciplinas Optativas: " + disciplinasOptativas + "\n" +
+               curriculo + "\n";
     }
 }
